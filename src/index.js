6f640e5abfbb1271a6071f5a629824c19f9ac475
Siarhei <s.chaptsau@gmail.com>
2018-02-27 12:59:40 +0300

    Implemented function 49%

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 module.exports = function getZerosCount(number, base) {
+  const a = 256 / 2;
+  function isPrime(num) { // функция поиска простых чисел
+    for (let i = 2; i < num; i++) {
+      if (num % i == 0) return false;
+    }
+    return true;
+  }
+
+  var arr = [];
+  for (let i = 1; i < a; i++){
+    if(isPrime(i)) {
+      arr.push(i); // создали массив простых чисел по 127
+    }
+  }
+
+  var maxNum = base; // макс. число на которое делится базовая система без остатка
+  var len = arr.length;
+  for (let i = len - 1; i > 1; i--) {
+    if (base % i == 0) {
+      maxNum = i;
+      break;
+    }
+  }
+
   var count = 0;
   while (number != 0) {
-    number = Math.floor(number / base);
-    count += number;
-  }
+    number = Math.floor(number / maxNum);
+    count += number; // счётчик числа нулей
+  }  
   return count;
 }
